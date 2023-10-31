184da78629de433622ef84b8cf136814ba30a808
Xavier Jordan <xjord002@odu.edu>
2023-10-31 17:30:46 -0400

    Added project folder to git repo

---
+++ CS471Project/CPUScheduler/cpuSchedule.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+void readFile(int n);
+int arrTime[513];
+int burst[513];
+int prio[513];
+
+int main() {
+    int n;
+
+    return 0;
+}
+
+void readFile(int n) {
+    fstream fin;
+    fin.open("Datafile1.txt", ios::in);
+    
+}
