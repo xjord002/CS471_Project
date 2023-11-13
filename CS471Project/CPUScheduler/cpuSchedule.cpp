b7c0db20b55b1a7181887111afbff4b350e42b35
Xavier Jordan <xjord002@odu.edu>
2023-11-13 14:00:23 -0500

    Stuff

--- CS471Project/CPUScheduler/cpuSchedule.cpp
+++ CS471Project/CPUScheduler/cpuSchedule.cpp
@@ -3,22 +3,34 @@
 #include <fstream>
 #include <vector>
 #include <cmath>
+#include <queue>
 
 using namespace std;
 
-void readFile(int n);
-int arrTime[513];
-int burst[513];
-int prio[513];
-
 int main() {
-    int n;
+    ifstream fin("Datafile1.txt");
+    if(!fin) {
+        cerr << "Failed to open the text file." << endl;
+        return 1;
+    }
+
+    string header;
+    getline(fin, header);
+
+    int n = 513;
+    vector<int> arriveTime, burstLength, prio;
+    int a, b, p;
+    while(fin >> a >> b >> p) {
+        arriveTime.push_back(a);
+        burstLength.push_back(b);
+        prio.push_back(p);
+    }
+
+    for(size_t i = 0; i < n; i++) {
+        cout << "Arrival Time: " << setw(5) << arriveTime[i] << "  Burst Length: " << setw(3) << burstLength[i] << "  Priority: " << prio[i] << endl;
+    }
+
+    fin.close();
 
     return 0;
-}
-
-void readFile(int n) {
-    fstream fin;
-    fin.open("Datafile1.txt", ios::in);
-    
 }
