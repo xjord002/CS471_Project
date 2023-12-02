245e007cfb85d5ebbd06f4d3ee7777f2ce0d4011
Xavier Jordan <xjord002@odu.edu>
2023-12-02 14:31:38 -0500

    Testing the readme file

--- CS471Project/CPUScheduler/PRIO/priorityScheduling.cpp
+++ CS471Project/CPUScheduler/PRIO/priorityScheduling.cpp
@@ -3,14 +3,15 @@
 #include <fstream>
 #include <algorithm>
 #include <vector>
+#include <queue>
  
 using namespace std;
 
 struct Process {
-    double arrivalTime;
-    double burstLength;
+    int arrivalTime;
+    int burstLength;
     int prio;
-    double waitTime;
+    int waitTime;
 };
 
 // Function to read in the Datafile1.txt file
@@ -33,7 +34,7 @@ void readFile(const string &fin, vector<Process> &processes) {
     // second column is burst length, and the last column is priority.
     while(inputFile >> process.arrivalTime >> process.burstLength >> process.prio) {
         // wait time is equal to 0
-        process.waitTime = 0.0;
+        process.waitTime = 0;
         // Add data to the end of the queue as they are read in.
         processes.push_back(process);
 
@@ -48,7 +49,7 @@ void readFile(const string &fin, vector<Process> &processes) {
 
 bool comparePrio(Process& a, Process& b) {
     if(a.arrivalTime == b.arrivalTime) {
-        return a.prio > b.prio;
+        return a.prio < b.prio;
     }
     return a.arrivalTime < b.arrivalTime;
 }
@@ -61,12 +62,12 @@ void PRIO(vector<Process> &processes) {
     double totalWaitTime = 0.0;
     double turnAroundTime = 0.0;
     double totalTurnAroundTime = 0.0;
-    double cumulativeWaitTime = 0.0;
-    double prevBurst = 0.0;
     sort(processes.begin(), processes.end(), comparePrio);
 
-    cout << "Arrival Time" << setw(16) << "Burst Length" << setw(15) << "Finish Time" 
-         << setw(20) << "Turn Around Time" << setw(13) << "Wait Time" << endl;
+    cout << "Arrival Time" << setw(16) << "Burst Length" << setw(15) << "Priority"
+         << setw(17) << "Finish Time" << setw(20) << "Turn Around Time" << setw(13) 
+         << "Wait Time" << endl;
+
     for(Process& process : processes) {
         if(process.arrivalTime > finishTime) {
             finishTime = process.arrivalTime;
@@ -74,11 +75,20 @@ void PRIO(vector<Process> &processes) {
         process.waitTime = finishTime - process.arrivalTime;
         finishTime += process.burstLength;
         turnAroundTime = finishTime - process.arrivalTime;
+        // process.waitTime = max(0.0, turnAroundTime - process.burstLength - process.arrivalTime);
         totalWaitTime += process.waitTime;
+        totalTurnAroundTime += turnAroundTime;
+        totalElapsedTime = max(totalElapsedTime, finishTime);
+        completedProcesses++;
         cout << setw(5) << process.arrivalTime << setw(17) << process.burstLength << setw(17) 
-             << finishTime << setw(17) << turnAroundTime
-             << setw(16) << process.waitTime << endl;
+                << process.prio << setw(17) << finishTime << setw(17) << turnAroundTime
+                << setw(16) << process.waitTime << endl;
     }
+    cout << "Total elapsed time: " << totalElapsedTime << endl;
+    cout << "Total Wait Time: " << totalWaitTime<< endl;
+    cout << "Completed Processes: " << completedProcesses << endl;
+    cout << "Average wait time: " << totalWaitTime / completedProcesses << endl;
+    cout << "Average Turn Around Time: " << totalTurnAroundTime / completedProcesses << endl;    
 }
  
 int main() {
--- CS471Project/CPUScheduler/README.md
+++ CS471Project/CPUScheduler/README.md
@@ -1,2 +1,16 @@
-** To execute the code:
-    ./cpuSchedule
+# CS471 Project - Problem 1
+
+## Authors
+Xavier Jordan
+
+## Description
+This program includes three scheduling algorithms which a user can choose from. They will act as a CPU scheduler that can handle 500 simulated processes. The following are the scheduling algorithms:
+    - First In First Out (FIFO)
+    - Shortes Job First without preemption (SJF)
+    - Priority with preemption
+
+## Input File
+
+
+## Running The Program
+The program should be run using the terminal with the following command:
